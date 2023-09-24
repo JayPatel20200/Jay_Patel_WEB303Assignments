169353a7b8546e61260dd1da0173151669b28dba
JayPatel@2024 <125040098+JayPatel20200@users.noreply.github.com>
2023-09-24 21:02:10 +0000

    Update styles.css and main.js

--- js/main.js
+++ js/main.js
@@ -1,2 +1,34 @@
 // WEB303 Assignment 2
-console.log("Hello world!");
+$(document).ready(function () {
+  const content = $("#content");
+
+  function handleLoadContent(page) {
+    const xhttp = new XMLHttpRequest();
+    xhttp.open("GET", page, true);
+
+    xhttp.onreadystatechange = function () {
+      if (this.readyState == 4 && this.status == 200) {
+        content.fadeOut(400, function () {
+          content.html(xhttp.responseText);
+          content.fadeIn(400);
+        });
+      }
+    };
+    xhttp.send();
+  }
+
+  $("#prospect").click(function () {
+    content.empty();
+    handleLoadContent("prospect.html");
+  });
+
+  $("#convert").click(function () {
+    content.empty();
+    handleLoadContent("convert.html");
+  });
+
+  $("#retain").click(function () {
+    content.empty();
+    handleLoadContent("retain.html");
+  });
+});
